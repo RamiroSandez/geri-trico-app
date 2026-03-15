f1c8e0b494924f78274e615798f1547a6ec6a236
Rama <ramirosandez12@gmail.com>
2026-03-14 21:17:24 -0300

    .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mfbltxyoyqsfdonghohu.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mYmx0eHlveXFzZmRvbmdob2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNTQxMjgsImV4cCI6MjA4ODgzMDEyOH0.uaRgnFcbNjQy2HIDaykWM-HkKcIn7UP4NffSMy7E1vc"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
